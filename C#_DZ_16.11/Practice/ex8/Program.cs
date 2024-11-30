7beaf235ed00550da810c5d5c464806ccce6e2ba
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-30 14:46:15 +0200

    Add files via upload

---
+++ C#_DZ_16.11/Practice/ex8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter sentence:");
+        string input = Console.ReadLine();
+        string vowels = "aeiouAEIOU";
+        int vowelCount = 0;
+        foreach (char c in input)
+        {
+            if (vowels.Contains(c))
+            {
+                vowelCount++;
+            }
+        }
+        Console.WriteLine($"Count of vowels in sentence: {vowelCount}");
+    }
+}
