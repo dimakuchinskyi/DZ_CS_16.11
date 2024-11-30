c8f91c6e3cb323f9282a6cec63bcef2355dafedc
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-30 14:49:27 +0200

    Add files via upload

---
+++ C#_DZ_16.11/Practice/ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter sentence:");
+        string inputString = Console.ReadLine();
+        Console.WriteLine("Enter word for search:");
+        string searchWord = Console.ReadLine();
+        int count = 0;
+        int startIndex = 0;
+        while ((startIndex = inputString.IndexOf(searchWord, startIndex, StringComparison.OrdinalIgnoreCase)) != -1)
+        {
+            count++;
+            startIndex += searchWord.Length;
+        }
+        Console.WriteLine($"Number of occurrences of a substring '{searchWord}': {count}");
+    }
+}
