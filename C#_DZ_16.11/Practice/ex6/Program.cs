72afcc7c4810359a589c328957054c7df157a0f4
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-30 14:11:46 +0200

    Add files via upload

---
+++ C#_DZ_16.11/Practice/ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ex6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string input = Console.ReadLine();
+
+            string[] word = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Count of word in sentence: " + word.Length );
+        }
+    }
+}
