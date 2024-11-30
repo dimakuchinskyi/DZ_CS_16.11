3b3240295680c8d81b798aee495912900558cecc
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-30 14:40:23 +0200

    Add files via upload

---
+++ C#_DZ_16.11/Practice/ex7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter sentence:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] wordArray = words[i].ToCharArray();
+            Array.Reverse(wordArray);
+            words[i] = new string(wordArray);
+        }
+        Console.WriteLine("The result after the coup: " + string.Join(" ", words));
+    }
+}
