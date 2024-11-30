24ea90e0a44d50df1a46cf4e23838f19ba10620b
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-30 13:52:30 +0200

    Add files via upload

---
+++ C#_DZ_16.11/Practice/ex5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp5;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        int[,] mas1 =
+            {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int min = mas1[0, 0];
+        int max = mas1[0, 0];
+        foreach (int i in mas1)
+        {
+            if(i < min)
+            {
+                min = i;
+            }
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        Console.WriteLine("Минимальное значение: " + min);
+        Console.WriteLine("Максимальное значение: " + max);
+    }
+    }
+
+
