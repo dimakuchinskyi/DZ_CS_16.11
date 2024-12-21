37af7d101c6aa76eed564770a26669c9937e72f8
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-12-21 13:08:36 +0200

    Add files via upload

---
+++ C#_DZ_16.11/hw/ex7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть текст:");
+            string inputText = Console.ReadLine();
+
+            Console.WriteLine("Введіть неприпустиме слово:");
+            string forbiddenWord = Console.ReadLine();
+
+            int replacements = 0;
+
+            string resultText = inputText;
+            while (resultText.Contains(forbiddenWord))
+            {
+                resultText = resultText.Replace(forbiddenWord, new string('*', forbiddenWord.Length));
+                replacements++;
+            }
+
+            Console.WriteLine("\nРезультат:");
+            Console.WriteLine(resultText);
+
+            Console.WriteLine($"\nСтатистика: {replacements} заміни слова {forbiddenWord}.");
+        }
+    }
+}
